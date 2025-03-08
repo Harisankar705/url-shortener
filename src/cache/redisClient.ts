d8332d9ef6f6576a598eeb04c2fc1471f8f4ef3d
harisankar <harisankar705@gmail.com>
2025-03-08 13:50:01 +0530

    committed

--- src/cache/redisClient.ts
+++ src/cache/redisClient.ts
@@ -1,12 +1,11 @@
 import { Redis } from 'ioredis'; 
 import dotenv from 'dotenv';
-console.log('Attempting to connect to Redis at:', process.env.REDIS_HOST, process.env.REDIS_PORT);
-
 
 dotenv.config();
-
+console.log('REDIS_HOST:', process.env.REDIS_HOST);
+console.log('REDIS_PORT:', process.env.REDIS_PORT);
 const redis = new Redis({
-  host: process.env.REDIS_HOST ,  
+  host: process.env.REDIS_HOST ,
   port: Number(process.env.REDIS_PORT) || 6379,
   maxRetriesPerRequest: null, 
   retryStrategy: (times) => Math.min(times * 50, 2000),
